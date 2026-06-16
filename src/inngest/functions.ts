8f4ba6d7b1cdb610b3ea517b616c7b2a28e524ff
A Naveen Kumar <naveenkumar.a2024@vitstudent.ac.in>
2026-06-16 16:13:39 +0530

    implementation of e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,31 @@
 // src/inngest/functions.ts
 import { inngest } from "./client";
-
+import { createAgent, gemini } from '@inngest/agent-kit';
+import { Sandbox } from "e2b";
+import { getSandbox } from "./utils";
 export const processTask = inngest.createFunction(
   { id: "process-task", triggers: { event: "app/task.created" } },
   async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("coden_agent-dev-2");
+      return sandbox.sandboxId;
+    });
+    const codeagent = createAgent({
+      name: 'Code assistant',
+      description: 'Provides expert support for writing and debugging code',
+      system:
+        'You are a PostgreSQL expert database administrator. ' +
+        'You only provide answers to questions related to PostgreSQL database schema, indexes, and extensions.',
+      model: gemini({
+        model: 'gemini-1.5-flash'
+      }),
     });
 
-    await step.sleep("pause", "10s");
-
-    return { message: `Task ${event.data.id} complete`, result };
+    const geturl = await step.run("get-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `http://${host}`;
+    });
+    return { output: `The sandbox is running at ${geturl}` };
   }
 );
